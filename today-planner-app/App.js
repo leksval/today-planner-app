7a0f36689008100a165530d5486d0ef4bd67aff8
kammic94 <kamil.michalik.94@gmail.com>
2019-05-18 14:13:45 +0200

    Update App.js

--- today-planner-app/App.js
+++ today-planner-app/App.js
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {Component} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import ForecastCard from './components/ForecastCard';
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			latitude: 0,
+			longitude: 0,
+			forecast: [],
+			error:''
+		};
+	}
+
+	componentDidMount(){
+		// Get the user's location
+		this.getLocation();
+	}
+
+	getLocation(){
+
+		// Get the current position of the user
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState(
+					(prevState) => ({
+					latitude: position.coords.latitude, 
+					longitude: position.coords.longitude
+					}), () => { this.getWeather(); }
+				);
+			},
+			(error) => this.setState({ forecast: error.message }),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+		);
+	}
+
+	getWeather(){
+
+		// Construct the API url to call
+		let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=c6123982940d7b0a7f2e210ce3b0dd95';
+
+		// Call the API, and set the state of the weather forecast
+		fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			this.setState((prevState, props) => ({
+				forecast: data
+			}));
+		})
+	}
+
+	render() {
+		return (
+			<FlatList data={this.state.forecast.list} style={{marginTop:20}} keyExtractor={item => item.dt_txt} renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
+		);
+	}
+}
