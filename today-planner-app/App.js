9295a885bef32beb7a64855bc0e2648d7c2144cb
Bartosz Lenart <s200400@wizard.uek.krakow.pl>
2019-05-20 16:26:10 +0200

    added login screen, navigation and conected with firebase

--- today-planner-app/App.js
+++ today-planner-app/App.js
@@ -1,21 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-export default class App extends React.Component {
+import * as firebase from 'firebase';
+
+import SignInScreen from './screens/SignInScreen'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhpJspUGaxOSNO8Vr6H3a6L8zyRC9UGUw",
+  authDomain: "todayplannersm-9880d.firebaseapp.com",
+  databaseURL: "https://todayplannersm-9880d.firebaseio.com",
+  projectId: "todayplannersm-9880d",
+  storageBucket: "todayplannersm-9880d.appspot.com",
+  messagingSenderId: "749997580651",
+  appId: "1:749997580651:web:9feccf4875cd0cf2"
+};
+firebase.initializeApp(firebaseConfig);
+
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Loadnig"
+          onPress={() => this.props.navigation.navigate('SignIn')}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+    SignIn: SignInScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+
+
+
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
