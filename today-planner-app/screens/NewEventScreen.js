5b509d4b60187877ef24849c5baafb90d890d075
Dulet <duleet@gmail.com>
2019-05-23 19:32:22 +0200

    small update

--- today-planner-app/screens/NewEventScreen.js
+++ today-planner-app/screens/NewEventScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text,Image, TouchableHighlight} from 'react-native';
 import * as firebase from 'firebase';
+import { Calendar, CalendarList, Agenda, Arrow } from 'react-native-calendars';
 import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
 
 
@@ -34,6 +35,49 @@ render() {
         <Container style={styles.container}>
             <Text>NewEvent screen</Text><Text>{"\n"}</Text>
             <Button title="Files" onPress={() => this.props.navigation.navigate('Files')}><Text>Files screen</Text></Button>
+            <CalendarList
+          // Initially visible month. Default = Date()
+          current={Date()}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={Date()}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2030-05-30'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {console.log('selected day', day)}}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={(day) => {console.log('selected day', day)}}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {console.log('month changed', month)}}
+          // Hide month navigation arrows. Default = false
+          hideArrows={true}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={(direction) => (<Arrow />)}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Hide day names. Default = false
+          hideDayNames={true}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={true}
+          horizontal={true}
+          pagingEnabled={true}
+          theme={{
+            arrowColor: 'white',
+            'stylesheet.calendar.header': {
+              week: {
+                marginTop: 5,
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+              }
+            }
+          }}
+        />
         </Container>
     );
 }
--- today-planner-app/screens/PlanScreen.js
+++ today-planner-app/screens/PlanScreen.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text,Image, TouchableHighlight} from 'react-native';
 import * as firebase from 'firebase';
-import { CalendarList } from 'react-native-calendars'
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
-
+import { Container, Content, Header, Form, Input, Item, Button, Label, TextInput } from 'native-base';
 
 
 export default class PlanScreen extends React.Component {
   constructor(props) {
     super(props)
-
 }
+
 static navigationOptions = () => ({
     title: 'TO-DAY',  
     headerTintColor: '#ae0578',
@@ -30,30 +28,67 @@ static navigationOptions = () => ({
   });
 
 
-// render() {
-//     return (
-//         <Container style={styles.container}>
-//             <Text>Plan screen</Text><Text>{"\n"}</Text>
-//             <Button title="NewEventButton (przerobić na floating '+' button)" onPress={() => this.props.navigation.navigate('NewEvent')}><Text>NewEvent screen</Text></Button><Text>{"\n"}</Text>
-//             <Button title="EventButton" onPress={() => this.props.navigation.navigate('Event')}><Text>Event screen</Text></Button>
-//         </Container>
-//     );
-// }
-// }
-
-render(){
+render() {
     return (
         <Container style={styles.container}>
-        <CalendarList
-        horizontal={true}
-        pagingEnabled={true}
-        calendarWidth={320}
-        />
+            <Text>Plan screen</Text><Text>{"\n"}</Text>
+            <Button title="NewEventButton (przerobić na floating '+' button)" onPress={() => this.props.navigation.navigate('NewEvent')}><Text>NewEvent screen</Text></Button><Text>{"\n"}</Text>
+            <Button title="EventButton" onPress={() => this.props.navigation.navigate('Event')}><Text>Event screen</Text></Button>
         </Container>
-        
-    )
+    );
 }
 }
+
+// render(){
+//     return (
+//         <Container style={styles.container}>
+//           <CalendarList
+//           // Initially visible month. Default = Date()
+//           current={Date()}
+//           // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+//           minDate={Date()}
+//           // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+//           maxDate={'2030-05-30'}
+//           // Handler which gets executed on day press. Default = undefined
+//           onDayPress={(day) => {console.log('selected day', day)}}
+//           // Handler which gets executed on day long press. Default = undefined
+//           onDayLongPress={(day) => {console.log('selected day', day)}}
+//           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+//           monthFormat={'MMMM yyyy'}
+//           // Handler which gets executed when visible month changes in calendar. Default = undefined
+//           onMonthChange={(month) => {console.log('month changed', month)}}
+//           // Hide month navigation arrows. Default = false
+//           hideArrows={true}
+//           // Replace default arrows with custom ones (direction can be 'left' or 'right')
+//           renderArrow={(direction) => (<Arrow />)}
+//           // Do not show days of other months in month page. Default = false
+//           hideExtraDays={true}
+//           // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+//           // day from another month that is visible in calendar page. Default = false
+//           disableMonthChange={true}
+//           // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+//           firstDay={1}
+//           // Hide day names. Default = false
+//           hideDayNames={true}
+//           // Show week numbers to the left. Default = false
+//           showWeekNumbers={true}
+//           horizontal={true}
+//           pagingEnabled={true}
+//           theme={{
+//             arrowColor: 'white',
+//             'stylesheet.calendar.header': {
+//               week: {
+//                 marginTop: 5,
+//                 flexDirection: 'row',
+//                 justifyContent: 'space-between'
+//               }
+//             }
+//           }}
+//         />
+//         </Container> 
+//     )
+// }
+// }
 
 const styles = StyleSheet.create({
 container: {
