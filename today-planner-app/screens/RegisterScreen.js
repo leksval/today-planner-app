1f015aad52b069d44804dd5e20bd7f4f4a5335d3
Bartosz Lenart <s200400@wizard.uek.krakow.pl>
2019-05-24 00:36:56 +0200

    Registration and sing in front end ready

--- today-planner-app/screens/RegisterScreen.js
+++ today-planner-app/screens/RegisterScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text} from 'react-native';
-import * as firebase from 'firebase';
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
+import { Text, View, Image} from 'react-native';
+import { Form, Input, Item, Button, Label } from 'native-base';
 
 
 
@@ -18,7 +17,6 @@ export default class SignInScreen extends React.Component {
 
 signUpUser = (email, password) => {
     try {
-
         if (this.state.password.length < 6) {
             alert("Please enter at least 6 characters")
             return;
@@ -27,7 +25,6 @@ signUpUser = (email, password) => {
             alert("Passwords does not match")
             return;
         }
-
         firebase.auth().createUserWithEmailAndPassword(email, password)
     }
     catch (error) {
@@ -37,63 +34,92 @@ signUpUser = (email, password) => {
 
 render() {
     return (
-        <Container style={styles.container}>
-            <Form>
+
+<View style={{
+        flex: 1,
+        width: 360,
+        height: 640,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{
+          flex: 1,
+          width: 360,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center',         
+          flexGrow: 1,
+          backgroundColor : '#032e5e'
+        }} >
+          <Image source={require('../assets/iconToday.png')} />
+        </View>
+        <View style={{
+          flex: 1,
+          width: 360,
+          height: 100,
+         
+          flexGrow: 2,
+          paddingHorizontal: 20               
+        }} >
+        {this.state.errorMessage &&
+          <Text style={{ color: 'red' }}>
+            {this.state.errorMessage}
+          </Text>}
+          
+          <Form>
                 <Item floatingLabel>
-                    <Label style={{color:'white'}}>Email</Label>
+                    <Label style={{color:'#ae0578'}}>email</Label>
                     <Input
                         autoCorrect={false}
                         autoCapitalize="none"
-                        style={{ color: 'white' }}
+                        style={{ color: '#032e5e' }}
                         onChangeText={(email) => this.setState({ email })}
                     />
 
                 </Item>
 
                 <Item floatingLabel>
-                    <Label style={{color:'white'}}>Password</Label>
+                    <Label style={{color:'#ae0578'}}>password</Label>
                     <Input
                         secureTextEntry={true}
                         autoCorrect={false}
                         autoCapitalize="none"
-                        style={{ color: 'white' }}
+                        style={{ color: '#032e5e' }}
                         onChangeText={(password) => this.setState({ password })}
                     />
                 </Item>
                 <Item floatingLabel>
-                    <Label style={{color:'white'}}>Confirm Password</Label>
+                    <Label style={{color:'#ae0578'}}>confirm password</Label>
                     <Input
                         secureTextEntry={true}
                         autoCorrect={false}
                         autoCapitalize="none"
-                        style={{ color: 'white' }}
+                        style={{ color: '#032e5e' }}
                         onChangeText={(confirmedPass) => this.setState({ confirmedPass })}
                     />
-                </Item>
-
-                <Button style={{ marginTop: 10 }}
+                </Item>           
+                <Button 
+                    style={{ marginTop: 60,
+                             backgroundColor:'#032e5e'}}
                     full
-                    rounded
                     primary
-                    onPress={ () => this.signUpUser(this.state.email, this.state.password)}
-                      
-                >
-                    <Text style={{ color: 'white' }}> Sign Up</Text>
-                </Button>
+                    onPress={ () => this.signUpUser(this.state.email, this.state.password)}>
+                    <Text style={{ color: '#d41998', fontWeight: 'bold', fontSize: 15}}> SIGN UP </Text>
+                </Button>           
             </Form>
-            <Button onPress={() => this.props.navigation.navigate('SignIn')}>
-          <Text>Already have an account? Sign In</Text>       
+            
+            <Button 
+            style={{ marginTop: 30, 
+                     backgroundColor:'#032e5e'}}
+             full
+             primary
+             onPress={() => this.props.navigation.navigate('SignIn')}>
+            <Text style={{ color: '#d41998', fontWeight: 'bold', fontSize: 15}}>ALREADY HAVE AN ACCOUNT? SIGN IN </Text>       
           </Button>
-        </Container>
+          
+        </View>
+      </View>
+
     );
 }
 }
-
-const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#032e5e',
-    justifyContent: 'center',
-    padding: 10
-},
-});
