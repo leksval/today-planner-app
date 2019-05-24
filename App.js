860aa881e2b91ef4d51b6311377ffb7631684888
kammic94 <kamil.michalik.94@gmail.com>
2019-05-24 22:30:13 +0200

    Add files via upload
    Napisałem kod, który w teorii ma pozwalać na tworzenie, wysyłanie ankiet przez smsy ( na podstawie tego co zrobił Paweł) i pobieranie informacji o oddanych głosach. Jedyny problem jest z API, które nie działa. Nie znalazłem żadnego zamiennika. W planach miałem też zapisywanie id utworzonych ankiet i przechowywanie ich na urządzeniu, ale nie miałem jak tego sprawdzić.

---
+++ App.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { FlatList, Text, Button, View, StyleSheet, TouchableOpacity, TextInput, Alert} from 'react-native';
+import PollData from './components/PollData';
+import Communications from 'react-native-communications';
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+     this.state = {
+        arrayHolder: [],
+        title: " ",
+        options: " ",
+        multi: " ",
+        idpoll: '',
+        pollData: []
+     }
+  } 
+  joinData = () => {
+    this.array.push({id : this.state.idpoll}); 
+    this.setState({ arrayHolder: [...this.array] })
+  }
+ sendPoll(title, options, multi) {
+   let url ='https://www.strawpoll.me/api/v2/polls{"title": '+{title}+',options:['+{options}+'],multi:'+{multi}+'';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState(prevState => ({
+          idpoll: data.id,
+         }),
+          () => {
+            this.joinData();
+          }
+          );
+  },
+      error => this.setState({}), 
+      {});
+  }
+   FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+      />
+    );
+  }
+  GetItem(item) {
+ 
+    Alert.alert(item);
+  }
+   getPolls(idpoll) {
+    let url =
+      'https://www.strawpoll.me/api/v2/polls/'+{idpoll}+'';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState((prevState, props) => ({
+          polldata: data,
+        }));
+      });
+  }
+  render() {
+    return (
+      <React.Fragment>
+         <View style={styles.container}>
+          <Text style={styles.buttonfor}>Fill all and press "Create poll"</Text>
+           <TextInput
+          style={{height: 40}}
+          placeholder="Type the question"
+          onChangeText={(title) => this.setState({title})}
+        />
+          <TextInput
+          style={{height: 40}}
+          placeholder="Type answers like shown below"
+          onChangeText={(options) => this.setState({options})}
+        />
+         <Text>"answer1","answer2"</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type true or false - multi choice"
+          onChangeText={(multi) => this.setState({multi})}
+        />
+        <TouchableOpacity onPress={this.sendPoll(this.state.title, this.state.options, this.state.multi)} activeOpacity={0.7} style={styles.button} >
+        <Text style={styles.buttonfor}> Create Poll </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress= {() => Communications.text('', 'Cześć,'+' prosze oddaj swój głos tutaj: https://www.strawpoll.me/'+this.props.idpoll)} style={styles.button} > 
+        <Text style={styles.buttonfor}>Send invite</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.getPolls()} activeOpacity={0.7} style={styles.button} > 
+        <Text style={styles.buttonfor}> Get My Polls Data </Text>
+        </TouchableOpacity>
+        <FlatList
+          data={this.state.pollData.list}
+          extraData={this.state.pollData}
+          style={{ marginTop: 20 }}
+          keyExtractor={item => item.dt_txt}
+          renderItem={({ item }) => (
+            <PollData
+              detail={item}
+              location={this.state.pollData.id}
+            />
+          )}
+        />
+        </View>
+      </React.Fragment>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  buttonfor: {
+    backgroundColor: 'purple',
+    borderColor: 'black',
+    borderWidth: 0,
+    borderRadius: 0,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    padding: 12,
+    textAlign:'center',
+  },
+
+});
---
+++ components/PollData.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { Text, Card, Divider } from 'react-native-elements';
+
+
+export default class ForecastCard extends Component {
+  	render() {
+		return (
+			<Card containerStyle={styles.card}>
+				<Text style={styles.notes}>Poll Id: {this.props.id}</Text>
+				<View style={{flexDirection:'row', alignItems:'center'}}>
+				</View>			
+				<View style={{flexDirection:'row', justifyContent:'space-between'}}>
+					<Text style={styles.notes}>Title: {this.props.title}</Text>
+          <Text style={styles.notes}>Answers: {this.props.options}</Text>
+          <Text style={styles.notes}>Votes: {this.props.votes}</Text>
+				</View>
+			</Card>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	card:{
+		backgroundColor:'rgba(56, 172, 236, 1)',
+		borderWidth:1,
+		borderRadius:1
+	},
+	notes: {
+		fontSize: 20,
+		color:'#fff',
+		textTransform:'capitalize'
+	}
+});
