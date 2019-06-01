0b78378cf93702a8a0fc39b20ccc66d39550e028
kammic94 <kamil.michalik.94@gmail.com>
2019-06-01 20:07:01 +0200

    Add files via upload

--- today-planner-app/screens/ForecastScreen.js
+++ today-planner-app/screens/ForecastScreen.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text,Image, TouchableHighlight} from 'react-native';
+import { StyleSheet, Text,Image, TouchableHighlight, FlatList, View, Animated} from 'react-native';
 import * as firebase from 'firebase';
 import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
-
+import ForecastCard from '../components/ForecastCard';
 
 
 export default class ForecastScreen extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      forecastData: this.props.navigation.state.params.forecast,
+    };
 
 }
-
-static navigationOptions = () => ({ 
+static navigationOptions = () => ({
+    title: 'TO-DAY',  
     headerTintColor: '#ae0578',
     headerStyle: {
       backgroundColor: '#032e5e'
     },
-  
-  headerRight:
-  <Image 
-      style={{flex:1, height: 100, width: 100, marginRight: 10}}
-      source={require('../assets/iconToday.png')} 
-      resizeMode="contain" />
+    headerLeft:
+    <TouchableHighlight onPress={() => this.props.navigation.navigate('Home')}>
+    <Image 
+        style={{flex:1, height: 100, width: 100}}
+        source={require('../assets/icon.png')} 
+        resizeMode="contain" />
+ </TouchableHighlight>
+  //  headerRight:
+  //    <HeaderBarItem to='FeedbackScreen' title='Feedback' />
+      
   });
+
 
 render() {
     return (
         <Container style={styles.container}>
-            <Text>Forecast screen</Text>
+         <FlatList
+          data={this.state.forecastData.list}
+          style={{ marginTop: 20 }}
+          keyExtractor={item => item.dt_txt}
+          renderItem={({ item }) => (
+            <ForecastCard
+              detail={item}
+              location={this.state.forecastData.city.name}
+            />
+          )}
+        />
         </Container>
     );
 }
